59866eae43e291015e8fa07d72910dfde4e1ede7
dt2g23 <dt2g23@soton.ac.uk>
2024-06-24 23:06:15 +0100

    Started chat application using React and Firebase, Currently creating the chatList, almost finished.

---
+++ src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import Userinfo from "../detail/userInfo/Userinfo";
+import ChatList from "./chatList/ChatList";
+import "./list.css";
+
+const List = () => {
+    return (
+        <div className='list'>
+            <Userinfo />
+            <ChatList />
+        </div>
+    )
+}
+
+export default List
