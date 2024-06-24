ba8061f2ac657431826ca6c258671f341653c6ac
dt2g23 <dt2g23@soton.ac.uk>
2024-06-24 23:59:21 +0100

    Chat list completed and completed a chunk of the actual chat

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -34,6 +34,20 @@ const ChatList = () => {
                     <p>Hello</p>
                 </div>
             </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
         </div>
     )
 }
