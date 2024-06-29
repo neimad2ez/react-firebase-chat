fa4eaafd7db7d05abc8efd832609b649aba0afd6
dt2g23 <dt2g23@soton.ac.uk>
2024-06-29 19:09:36 +0100

    Created upload.js which handles storing images copied from firebase docs. Able to store images now

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify"
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
+import upload from "../../lib/upload";
 
 const Login = () => {
     const [avatar, setAvatar] = useState({
@@ -25,9 +26,12 @@ const Login = () => {
 
             const res = await createUserWithEmailAndPassword(auth,email,password)
 
+            const imgUrl = await upload(avatar.file)
+
             await setDoc(doc(db, "users", res.user.uid), {
                 username,
                 email,
+                avatar: imgUrl,
                 id: res.user.uid,
                 blocked: [],
             });
